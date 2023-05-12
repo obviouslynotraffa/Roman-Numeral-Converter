3eb1667c67127cf19ae359ead7d9d3b568757b6e
raffaele <raffaele.fontana.02@gmail.com>
2023-05-12 18:43:12 +0200

    added class interface
    close #4

---
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -0,0 +1,17 @@
+
+
+
+package it.unipd.mtss;
+
+public class RomanPrinter {
+
+  public static String print(int num){
+  return printAsciiArt(IntegerToRoman.convert(num));
+  }
+
+  
+  private static String printAsciiArt(String romanNumber){
+  //TODO
+  return null;
+  }
+}
