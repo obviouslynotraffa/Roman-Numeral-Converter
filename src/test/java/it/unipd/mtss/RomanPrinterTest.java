573908350bfea9c6b6ca64b10463a2bae84fa66b
michele nesler <michele.nesler@studenti.unipd.it>
2023-05-13 17:14:15 +0200

    create test print I
    close #6

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -1,5 +1,27 @@
 package it.unipd.mtss;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
 public class RomanPrinterTest {
     
+    @Test
+    public void PrintIASCII() throws NegativeNumberException, ZeroException, 
+    BiggerThan5000Exception, NotRomanLetterException{
+        int number= 1;
+        String ascii= RomanPrinter.print(number);
+        String I =(
+            "  _____  \n"+
+            " |_   _| \n"+
+            "   | |   \n"+ 
+            "   | |   \n"+ 
+            "  _| |_  \n"+ 
+            " |_____| \n");
+            
+            assertEquals(I , ascii);
+    } 
+
+
 }
