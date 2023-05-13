816d5ae5f005759208759a616a2b3e0dc9d2f4d9
michele nesler <michele.nesler@studenti.unipd.it>
2023-05-13 17:27:31 +0200

    implement class IntegerToRoman
    close #16

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,8 +5,23 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-  public static String convert(int number){
-  // TODO
-  return null;
-  }
+  public static String convert(int number) throws ZeroException, NegativeNumberException, 
+  BiggerThan5000Exception{
+  
+    if(number==0) {throw new ZeroException(); }
+    if(number<0) {throw new NegativeNumberException();}
+    if(number>5000) {throw new BiggerThan5000Exception();}
+
+    
+    String[] thousands = {"", "M"};  
+    String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};  
+    String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};  
+    String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};  
+    
+    return thousands[number / 1000] + hundreds[(number % 1000) / 100] 
+        + tens[(number % 100) / 10] 
+        + units[number % 10];  
+    } 
+
+ 
 }
